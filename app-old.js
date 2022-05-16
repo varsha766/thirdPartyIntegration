d2c2d844b67d9d89155f42c89d9905c16055033f
Vishwas <vishwasanand.cse@gmail.com>
2022-05-16 14:36:14 +0530

    added a simple test

---
+++ app-old.js
@@ -0,0 +1,169 @@
+import env from "dotenv"
+import fetch from "node-fetch";
+import web3 from "web3";
+import express from "express";
+import mongoose from "mongoose";
+import http from "http";
+import path from 'path';
+import UserModel from "./userDetail.js"
+const __dirname = path.resolve();
+
+env.config();
+const app = express();
+const server = http.createServer(app);
+const dbUrl = process.env.DB_URL
+const privateKey = process.env.PRIVATEKEY
+const base_url = process.env.HYPERFYRE_BASE_URL
+const appId = process.env.APP_ID
+const port = process.env.PORT
+mongoose.connect(dbUrl)
+    .then((result) => {
+        console.log("connected successfully!");
+    })
+    .catch((err) => {
+        console.log(err)
+    });
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+
+//function to generate signature
+
+const SignData = async(Json_data, privateKey) => {
+    const Web3 = new web3();
+    const signedData = await Web3.eth.accounts.sign(JSON.stringify(Json_data), privateKey)
+    return signedData
+}
+app.get("/", async(req, res) => {
+    try {
+        res.sendFile("./public/login.html", { root: __dirname });
+    } catch (e) {
+        res.send(`Error: ${e}`);
+    }
+})
+app.get('/home', (req, res) => {
+    res.sendFile("./public/home.html", { root: __dirname })
+})
+app.get('/marketplace', (req, res) => {
+    res.sendFile("./public/marketplace.html", { root: __dirname })
+})
+app.get('/profile', (req, res) => {
+    res.sendFile("./public/profile.html", { root: __dirname })
+})
+app.post('/', async(req, res) => {
+    try {
+        let userDetail;
+        userDetail = await UserModel.findOne({ publicKey: req.body })
+        if (!userDetail) {
+            userDetail = await UserModel.create({
+                publicKey: req.body
+            })
+        }
+        externalUserId = userDetail.publicKey;
+        console.log(externalUserId)
+        res.json(userDetail)
+    } catch (e) {
+        res.send(e)
+    }
+})
+
+app.post('/event', async(req, res) => {
+    try {
+        const path = '/ext/api/v1/app/events'
+        req.body.message['appId'] = appId
+        const payload = req.body
+
+        const signature = await SignData(payload.message, privateKey)
+        payload['fyresign'] = signature.signature
+        payload['messageHash'] = signature.messageHash
+
+        console.log({
+            base_url,
+            path,
+            payload
+        })
+        const response = await fetch(base_url + path, {
+
+            headers: {
+                "Content-Type": 'application/json',
+                "Origin": "http://localhost:3000"
+            },
+            method: 'POST',
+            body: JSON.stringify(payload)
+        })
+        const json = await response.json()
+        console.log(json)
+        res.send(json)
+
+    } catch (e) {
+        console.log(`Error: ${e}`);
+        res.send(e)
+    }
+})
+app.post('/user/event', async(req, res) => {
+    try {
+        const url = '/ext/api/v1/app/user/events'
+        req.body.message['appId'] = appId
+        const payload = req.body
+        const signature = await SignData(payload.message, privateKey)
+        payload['fyresign'] = signature.signature
+        payload['messageHash'] = signature.messageHash
+
+        console.log({
+            payload,
+            base_url,
+            url
+        });
+        const resp = await fetch(base_url + url, {
+            headers: {
+                "Content-Type": 'application/json',
+                "Origin": "http://localhost:3000"
+
+            },
+            method: 'POST',
+            body: JSON.stringify(payload)
+        })
+        console.log(resp)
+        const json = await resp.json()
+        console.log(json)
+        res.send(json);
+
+    } catch (e) {
+        console.log(`Error: ${e}`);
+        res.send(e.messsage)
+    }
+})
+
+app.post('/token', async(req, res) => {
+    try {
+
+        const url = '/ext/api/v1/app/user/redirection'
+        req.body.message['appId'] = appId
+        const payload = req.body
+        const signature = await SignData(payload.message, privateKey)
+        payload['fyresign'] = signature.signature
+        payload['messageHash'] = signature.messageHash
+
+        const response = await fetch(base_url + url, {
+            headers: {
+                "Content-Type": 'application/json',
+                "Origin": "http://localhost:3000"
+            },
+            method: 'POST',
+            body: JSON.stringify(payload)
+        })
+        console.log(response.status)
+        const json = await response.json()
+        console.log(json)
+        res.send(json)
+
+
+    } catch (e) {
+        console.log(`Error: ${e}`);
+        res.send(e)
+    }
+})
+
+server.listen(port, () => {
+    console.log('server is running on  port http://localhost:' + port);
+})
--- app.js
+++ app.js
@@ -1,169 +1,68 @@
-import env from "dotenv"
-import fetch from "node-fetch";
-import web3 from "web3";
 import express from "express";
-import mongoose from "mongoose";
-import http from "http";
-import path from 'path';
-import UserModel from "./userDetail.js"
-const __dirname = path.resolve();
+import web3 from "web3";
+import fetch from "node-fetch";
+const port = 4000;
 
-env.config();
 const app = express();
-const server = http.createServer(app);
-const dbUrl = process.env.DB_URL
-const privateKey = process.env.PRIVATEKEY
-const base_url = process.env.HYPERFYRE_BASE_URL
-const appId = process.env.APP_ID
-const port = process.env.PORT
-mongoose.connect(dbUrl)
-    .then((result) => {
-        console.log("connected successfully!");
-    })
-    .catch((err) => {
-        console.log(err)
-    });
-app.use(express.static('public'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }))
+const credential = {
+    appWalletAddress: "0x32ac069d865E690CF163C6b9C2e63033b1d44eB8",
+    AppPrivateKey: "0x4c391814951c00a7b91a62f1802ce8747aa8fbd14466975afc1a768df809fe3e",
+    AppPublicKey: "0x02d09677f1cbaf88019ce3912fe8c0afca2b88ddd5bfdf18484af1bc26698c5f86",
+    RecoveryPhrase: "property inquiry neglect try dry gorilla genuine powder imitate symptom orange marble country congress until viable grape similar tunnel sleep mobile blanket notable advice",
+    AppId: "627a9f5b42ad5242e2f425de",
+};
 
-//function to generate signature
 
 const SignData = async(Json_data, privateKey) => {
     const Web3 = new web3();
     const signedData = await Web3.eth.accounts.sign(JSON.stringify(Json_data), privateKey)
     return signedData
 }
-app.get("/", async(req, res) => {
+
+
+const baseURl = 'http://localhost:6006'
+app.get("/events", async(req, res, next) => {
+    console.log('Isndie post events')
     try {
-        res.sendFile("./public/login.html", { root: __dirname });
-    } catch (e) {
-        res.send(`Error: ${e}`);
-    }
-})
-app.get('/home', (req, res) => {
-    res.sendFile("./public/home.html", { root: __dirname })
-})
-app.get('/marketplace', (req, res) => {
-    res.sendFile("./public/marketplace.html", { root: __dirname })
-})
-app.get('/profile', (req, res) => {
-    res.sendFile("./public/profile.html", { root: __dirname })
-})
-app.post('/', async(req, res) => {
-    try {
-        let userDetail;
-        userDetail = await UserModel.findOne({ publicKey: req.body })
-        if (!userDetail) {
-            userDetail = await UserModel.create({
-                publicKey: req.body
-            })
+        const url = baseURl + '/ext/api/v1/app/events'
+        let payload = {
+            "message": {
+                "appId": credential.AppId,
+                "iat": Math.floor(Date.now() / 1000),
+                "exp": Math.floor((Date.now() + (3600 * 24 * 1000)) / 1000),
+                "metaData": {
+                    "page": 1,
+                    "limit": 2
+                }
+            },
+            "fyresign": "",
+            "messageHash": ""
         }
-        externalUserId = userDetail.publicKey;
-        console.log(externalUserId)
-        res.json(userDetail)
-    } catch (e) {
-        res.send(e)
-    }
-})
 
-app.post('/event', async(req, res) => {
-    try {
-        const path = '/ext/api/v1/app/events'
-        req.body.message['appId'] = appId
-        const payload = req.body
-
-        const signature = await SignData(payload.message, privateKey)
+        const signature = await SignData(payload.message, credential.AppPrivateKey)
         payload['fyresign'] = signature.signature
         payload['messageHash'] = signature.messageHash
 
-        console.log({
-            base_url,
-            path,
-            payload
-        })
-        const response = await fetch(base_url + path, {
-
-            headers: {
-                "Content-Type": 'application/json',
-                "Origin": "http://localhost:3000"
-            },
+        console.log(payload)
+        const resp = await fetch(url, {
             method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Origin': 'http://localhost:3000'
+            },
             body: JSON.stringify(payload)
         })
-        const json = await response.json()
-        console.log(json)
-        res.send(json)
-
-    } catch (e) {
-        console.log(`Error: ${e}`);
-        res.send(e)
-    }
-})
-app.post('/user/event', async(req, res) => {
-    try {
-        const url = '/ext/api/v1/app/user/events'
-        req.body.message['appId'] = appId
-        const payload = req.body
-        const signature = await SignData(payload.message, privateKey)
-        payload['fyresign'] = signature.signature
-        payload['messageHash'] = signature.messageHash
-
-        console.log({
-            payload,
-            base_url,
-            url
-        });
-        const resp = await fetch(base_url + url, {
-            headers: {
-                "Content-Type": 'application/json',
-                "Origin": "http://localhost:3000"
-
-            },
-            method: 'POST',
-            body: JSON.stringify(payload)
-        })
-        console.log(resp)
         const json = await resp.json()
         console.log(json)
-        res.send(json);
-
+        res.status(200).json(json);
     } catch (e) {
-        console.log(`Error: ${e}`);
-        res.send(e.messsage)
-    }
-})
-
-app.post('/token', async(req, res) => {
-    try {
-
-        const url = '/ext/api/v1/app/user/redirection'
-        req.body.message['appId'] = appId
-        const payload = req.body
-        const signature = await SignData(payload.message, privateKey)
-        payload['fyresign'] = signature.signature
-        payload['messageHash'] = signature.messageHash
-
-        const response = await fetch(base_url + url, {
-            headers: {
-                "Content-Type": 'application/json',
-                "Origin": "http://localhost:3000"
-            },
-            method: 'POST',
-            body: JSON.stringify(payload)
+        console.log(e)
+        res.status(500).json({
+            error: e.message
         })
-        console.log(response.status)
-        const json = await response.json()
-        console.log(json)
-        res.send(json)
-
-
-    } catch (e) {
-        console.log(`Error: ${e}`);
-        res.send(e)
     }
-})
+});
 
-server.listen(port, () => {
-    console.log('server is running on  port http://localhost:' + port);
-})
+app.listen(port, () => {
+    console.log("This server is runng on port = ", port);
+});
